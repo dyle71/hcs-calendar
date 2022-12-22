c70a3866ca567d5b585cc7604a1df0b2ab686199
Oliver Maurhart <oliver.maurhart@headcode.space>
2022-12-22 15:47:36 +0100

    Renames CalendarBanner to CalendarHeader

--- src/CalendarView.vue
+++ src/CalendarView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import CalendarBanner from "@/components/CalendarBanner.vue";
+import CalendarHeader from "@/components/CalendarHeader.vue";
+import CalendarMain from "@/components/CalendarMain.vue";
 import CalendarFooter from "@/components/CalendarFooter.vue";
-import CalendarBody from "@/components/CalendarMain.vue";
 import CalendarSideBar from "@/components/CalendarSideBar.vue";
 </script>
 
 <template>
-  <CalendarBanner />
-  <CalendarBody />
+  <CalendarHeader />
+  <CalendarMain />
   <CalendarFooter />
   <CalendarSideBar />
 </template>
