9092ff863433429822238f19f0c4a70e9baf8eb0
Oliver Maurhart <oliver.maurhart@headcode.space>
2022-12-22 15:45:12 +0100

    Adds CalendarFooter template

--- src/CalendarView.vue
+++ src/CalendarView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import CalendarBanner from "@/components/CalendarBanner.vue";
+import CalendarFooter from "@/components/CalendarFooter.vue";
 import CalendarBody from "@/components/CalendarMain.vue";
 import CalendarSideBar from "@/components/CalendarSideBar.vue";
 </script>
 
 <template>
-  <div>
-    <CalendarBanner />
-    <CalendarBody />
-    <CalendarSideBar />
-  </div>
+  <CalendarBanner />
+  <CalendarBody />
+  <CalendarFooter />
+  <CalendarSideBar />
 </template>
 
 <style scoped></style>
