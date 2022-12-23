1ba837d22ec843b2572bfba5bf5ee009a7a7e618
Oliver Maurhart <oliver.maurhart@headcode.space>
2022-12-23 19:46:52 +0100

    Adds a Tooltip to header shift icons

--- src/components/CalendarHeader.vue
+++ src/components/CalendarHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MonthLabel from "@/components/elements/MonthLabel.vue";
 import ShiftButton from "@/components/elements/ShiftButton.vue";
+import ToolTip from "@/components/elements/ToolTip.vue";
 
 interface Props {
   date: number;
@@ -19,36 +20,53 @@ const emit = defineEmits([
 <template>
   <div class="calendar-header">
     <ShiftButton
+      id="calendar-header-double-left"
       class="calendar-header__shift-button"
       direction="double-left"
-      alt="{{ $t('header.shift.double-left') }}"
       @click="emit('onDoubleLeft')"
     />
+    <ToolTip for="calendar-header-double-left">
+      {{ $t("header.shift.double-left") }}
+    </ToolTip>
+
     <ShiftButton
+      id="calendar-header-left"
       class="calendar-header__shift-button"
       direction="left"
-      alt="{{ $t('header.shift.left') }}"
       @click="emit('onLeft')"
     />
+    <ToolTip for="calendar-header-left">
+      {{ $t("header.shift.left") }}
+    </ToolTip>
+
     <MonthLabel :date="props.date" />
+
     <ShiftButton
+      id="calendar-header-right"
       class="calendar-header__shift-button"
       direction="right"
-      alt="{{ $t('header.shift.right') }}"
       @click="emit('onRight')"
     />
+    <ToolTip for="calendar-header-right">
+      {{ $t("header.shift.right") }}
+    </ToolTip>
+
     <ShiftButton
+      id="calendar-header-double-right"
       class="calendar-header__shift-button"
       direction="double-right"
-      alt="{{ $t('header.shift.double-right') }}"
       @click="emit('onDoubleRight')"
     />
+    <ToolTip for="calendar-header-double-right">
+      {{ $t("header.shift.double-right") }}
+    </ToolTip>
+
   </div>
 </template>
 
 <style scoped>
 .calendar-header {
-  @apply relative flex;
+  @apply relative flex mx-auto;
 }
 
 .calendar-header__shift-button {
---
+++ src/components/elements/ToolTip.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { createPopper, Instance } from "@popperjs/core";
+
+interface Props {
+  for: string;
+}
+const props = withDefaults(defineProps<Props>(), {});
+
+const tooltipElement = ref<HTMLElement | null>(null);
+
+let popperInstance: Instance | null = null;
+let popperTarget: HTMLElement | null = null;
+
+function hideToolTip() {
+  if (tooltipElement.value) {
+    tooltipElement.value.removeAttribute("tooltip-active");
+
+    if (popperInstance) {
+      popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          ...options.modifiers!,
+          { name: "eventListeners", enabled: false },
+        ],
+      }));
+    }
+  }
+}
+
+function showToolTip() {
+  if (popperInstance && tooltipElement.value) {
+    tooltipElement.value.setAttribute("tooltip-active", "");
+
+    popperInstance.setOptions((options) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers!,
+        { name: "eventListeners", enabled: true },
+      ],
+    }));
+
+    popperInstance.update();
+  }
+}
+
+onMounted(() => {
+  popperTarget = document.getElementById(props.for);
+  if (!popperTarget) {
+    console.warn(
+      `Failed to produce a tooltip for ${props.for}, element not found.`
+    );
+    return;
+  }
+  if (!tooltipElement.value) {
+    console.warn(`Failed get ref of current tooltip (for: ${props.for}).`);
+    return;
+  }
+  popperInstance = createPopper(popperTarget, tooltipElement.value, {
+    placement: "auto",
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [0, 8],
+        },
+      },
+      {
+        name: "flip",
+        options: {
+          fallbackPlacements: ["top", "right", "bottom", "left"],
+        },
+      },
+    ],
+  });
+
+  popperTarget.onmouseenter = showToolTip;
+  popperTarget.onfocus = showToolTip;
+  popperTarget.onmouseleave = hideToolTip;
+  popperTarget.onblur = hideToolTip;
+});
+</script>
+
+<template>
+  <div ref="tooltipElement" class="tooltip" role="tooltip">
+    <slot />
+    <div class="tooltip__arrow" data-popper-arrow></div>
+  </div>
+</template>
+
+<style scoped>
+.tooltip {
+  @apply hidden font-bold px-2 py-1 rounded-md text-xs text-white opacity-0;
+  background: rgba(49, 49, 49);
+  z-index: 1000;
+}
+
+.tooltip[tooltip-active] {
+  @apply block;
+  animation-name: animate-tooltip;
+  animation-duration: 500ms;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in-out;
+  animation-delay: 250ms;
+  animation-fill-mode: forwards;
+}
+
+@keyframes animate-tooltip {
+  from {opacity: 0.0;}
+  to {opacity: 0.95;}
+}
+
+.tooltip__arrow,
+.tooltip__arrow::before {
+  @apply absolute;
+  width: 8px;
+  height: 8px;
+  background: inherit;
+}
+
+.tooltip__arrow {
+  visibility: hidden;
+}
+
+.tooltip__arrow::before {
+  visibility: visible;
+  content: "";
+  transform: rotate(45deg);
+}
+
+.tooltip[data-popper-placement^="top"] > .tooltip__arrow {
+  bottom: -4px;
+}
+
+.tooltip[data-popper-placement^="bottom"] > .tooltip__arrow {
+  top: -4px;
+}
+
+.tooltip[data-popper-placement^="left"] > .tooltip__arrow {
+  right: -4px;
+}
+
+.tooltip[data-popper-placement^="right"] > .tooltip__arrow {
+  left: -4px;
+}
+</style>
