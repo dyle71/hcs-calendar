e8a1a08d0f492bd4a2f759ea255be1a5472b91ad
Oliver Maurhart <oliver.maurhart@headcode.space>
2022-12-25 14:03:57 +0100

    Simplifies MonthLabel

--- src/components/elements/MonthLabel.vue
+++ src/components/elements/MonthLabel.vue
@@ -7,24 +7,9 @@ const props = withDefaults(defineProps<Props>(), {});
 </script>
 
 <template>
-  <div class="calendar-month-label">
-    <div class="calendar-month-label__month">
-      {{ $t(`month.long.${new Date(props.date).getMonth()}`) }}
-    </div>
-    <div class="calendar-month-label__year">
-      {{ new Date(props.date).getFullYear() }}
-    </div>
-  </div>
+  {{
+    $t(`month.long.${new Date(props.date).getMonth()}`) +
+    " " +
+    new Date(props.date).getFullYear()
+  }}
 </template>
-
-<style scoped>
-.calendar-month-label {
-  @apply py-2 px-2 text-xl flex flex-row gap-2;
-}
-
-.calendar-month-label__month {
-}
-
-.calendar-month-label__year {
-}
-</style>
