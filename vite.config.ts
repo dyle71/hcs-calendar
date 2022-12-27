3952ce11c4e17d82767ebb68b9835750c5f7dcd8
Oliver Maurhart <oliver.maurhart@headcode.space>
2022-12-27 16:42:26 +0100

    Fixes i18n ESM warnings

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 const projectRootDir = resolve(__dirname);
 
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue(), eslintPlugin(), VueI18nPlugin({})],
   resolve: {
     alias: {
       "@": resolve(projectRootDir, "src"),
